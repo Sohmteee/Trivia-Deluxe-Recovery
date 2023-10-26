18509a03f5e88533093d81a22bfbd030750dfa4c
Ukaegbe Somtochukwu <sohmteecodes@gmail.com>
2023-10-26 16:25:41 +0100

    commit

--- lib/models/dialogs/create_profile.dart
+++ lib/models/dialogs/create_profile.dart
@@ -172,11 +172,11 @@ showCreateProfileDialog(BuildContext context) {
               print(profileProvider.createPlayer());
               Navigator.pop(context);
             } else {
-              showGameDialog(context,
+              showGameDialog(
     context,
     isExitable: true,
     padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 40),
-    // margin: EdgeInsets.symmetric(horizontal: 60.w, vertical: 24.h), child: child)
+    margin: EdgeInsets.symmetric(horizontal: 60.w, vertical: 24.h), child: Column())
             }
           },
           child: Container(
