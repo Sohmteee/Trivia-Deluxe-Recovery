a9755b7d4fa548a96ffa5ae631e928331f5b4d34
Ukaegbe Somtochukwu <sohmteecodes@gmail.com>
2023-10-26 16:47:28 +0100

    commit

--- lib/providers/profile.dart
+++ lib/providers/profile.dart
@@ -1,4 +1,3 @@
-import 'package:device_info_plus/device_info_plus.dart';
 import 'package:flutter/material.dart';
 import 'package:trivia/data/box.dart';
 
@@ -6,9 +5,9 @@ class ProfileProvider extends ChangeNotifier {
   String? username = box.get("username", defaultValue: null);
   String? gender = box.get("gender", defaultValue: null);
 
-  createPlayer() async {
-    DeviceInfoPlugin deviceInfo = DeviceInfoPlugin();
+  createPlayer({required String username})  {
+    /* DeviceInfoPlugin deviceInfo = DeviceInfoPlugin();
     AndroidDeviceInfo androidInfo = await deviceInfo.androidInfo;
-    String? deviceID = androidInfo.id;
+    String? deviceID = androidInfo.id; */
   }
 }
