daac956d1068b26409a730934f63255ab93d28fd
Ukaegbe Somtochukwu <sohmteecodes@gmail.com>
2023-10-26 14:34:14 +0100

    commit

--- lib/models/dialogs/create_profile.dart
+++ lib/models/dialogs/create_profile.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:provider/provider.dart';
 import 'package:trivia/colors/app_color.dart';
 import 'package:trivia/main.dart';
 import 'package:trivia/providers/profile.dart';
