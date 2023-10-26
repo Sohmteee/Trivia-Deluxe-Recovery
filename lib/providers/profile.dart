392ff0be089e5eb5472a9c9deab70392924caa14
Ukaegbe Somtochukwu <sohmteecodes@gmail.com>
2023-10-26 16:18:44 +0100

    commit

--- lib/providers/profile.dart
+++ lib/providers/profile.dart
@@ -10,7 +10,5 @@ class ProfileProvider extends ChangeNotifier {
     DeviceInfoPlugin deviceInfo = DeviceInfoPlugin();
     AndroidDeviceInfo androidInfo = await deviceInfo.androidInfo;
     String? deviceID = androidInfo.id;
-
-    return deviceID;
   }
 }
