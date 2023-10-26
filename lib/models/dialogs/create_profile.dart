8c9ecff3c7daeaed04a962d7498415f2d6d29634
Ukaegbe Somtochukwu <sohmteecodes@gmail.com>
2023-10-26 21:22:14 +0100

    commit

--- lib/models/dialogs/create_profile.dart
+++ lib/models/dialogs/create_profile.dart
@@ -33,7 +33,7 @@ showCreateProfileDialog(BuildContext context) {
         Column(
           children: [
             Text(
-              "Gender",
+              "Choose and avatar",
               style: TextStyle(
                 color: AppColor.white,
                 fontSize: 20.sp,
@@ -56,8 +56,8 @@ showCreateProfileDialog(BuildContext context) {
                       });
                     },
                     child: Container(
-                      width: 40.sp,
-                      height: 40.sp,
+                      width: 50.sp,
+                      height: 50.sp,
                       decoration: BoxDecoration(
                         shape: BoxShape.circle,
                         image: DecorationImage(
@@ -79,104 +79,7 @@ showCreateProfileDialog(BuildContext context) {
           ],
         ),
         SizedBox(height: 20.h),
-        Row(
-          children: [
-            Expanded(
-              child: TextField(
-                controller: controller,
-                // autofocus: true,
-                style: TextStyle(
-                  color: AppColor.yellow,
-                  fontSize: 18.sp,
-                ),
-                onChanged: (value) {},
-                onSubmitted: (value) {
-                  playTap(context);
-                  if (controller.value.text.trim().isNotEmpty) {
-                    final profileProvider =
-                        Provider.of<ProfileProvider>(context, listen: false);
-                    profileProvider.createPlayer(
-                      context,
-                      username: controller.value.text.trim(),
-                    );
-                    Navigator.pop(context);
-                  } else {
-                    showGameDialog(
-                      context,
-                      isExitable: true,
-                      padding: const EdgeInsets.symmetric(
-                          horizontal: 20, vertical: 40),
-                      margin: EdgeInsets.symmetric(
-                          horizontal: 60.w, vertical: 24.h),
-                      child: Column(
-                        mainAxisSize: MainAxisSize.min,
-                        children: [
-                          Text(
-                            "Invalid Username",
-                            style: TextStyle(
-                              color: AppColor.slightlyLighterYellow,
-                              fontSize: 25.sp,
-                              fontWeight: FontWeight.bold,
-                            ),
-                            textAlign: TextAlign.center,
-                          ),
-                          SizedBox(height: 10.h),
-                          Text(
-                            "Please provide a valid username to continue",
-                            style: TextStyle(
-                              color: AppColor.white,
-                              fontSize: 18.sp,
-                              fontWeight: FontWeight.bold,
-                            ),
-                            textAlign: TextAlign.center,
-                          ),
-                          SizedBox(height: 30.h),
-                          ZoomTapAnimation(
-                            onTap: () {
-                              playTap(context);
-                              Navigator.pop(context);
-                            },
-                            child: Container(
-                              padding: EdgeInsets.symmetric(
-                                vertical: 10.sp,
-                                horizontal: 20.sp,
-                              ),
-                              decoration: BoxDecoration(
-                                color: Colors.white,
-                                borderRadius: BorderRadius.circular(20.r),
-                              ),
-                              child: Text(
-                                "Okay",
-                                style: TextStyle(
-                                  fontSize: 20.sp,
-                                ),
-                              ),
-                            ),
-                          )
-                        ],
-                      ),
-                    );
-                  }
-                },
-                textCapitalization: TextCapitalization.words,
-                // : TextCapitalization.none,
-                textInputAction: TextInputAction.done,
-                decoration: InputDecoration(
-                  labelText: "Username (Tap to edit)",
-                  labelStyle: TextStyle(
-                    color: AppColor.white,
-                    fontSize: 22.sp,
-                  ),
-                  border: InputBorder.none,
-                  counterStyle: const TextStyle(
-                    color: Colors.transparent,
-                  ),
-                ),
-                maxLength: 10,
-              ),
-            ),
-          ],
-        ),
+        
         SizedBox(height: 20.h),
         ZoomTapAnimation(
           onTap: () {
