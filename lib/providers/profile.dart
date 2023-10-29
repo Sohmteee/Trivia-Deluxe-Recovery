2fea1b11471f0534b489579b742ce65065c5d3a7
Ukaegbe Somtochukwu <sohmteecodes@gmail.com>
2023-10-29 17:17:00 +0100

    commit

--- lib/providers/profile.dart
+++ lib/providers/profile.dart
@@ -179,10 +179,116 @@ class ProfileProvider extends ChangeNotifier {
           );
         }).catchError((error) {
           print("Error: $error");
+
+          showGameDialog(
+            context,
+            isExitable: true,
+            padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 40),
+            margin: EdgeInsets.symmetric(horizontal: 60.w, vertical: 24.h),
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Text(
+                  "Error Creating Profile",
+                  style: TextStyle(
+                    color: AppColor.yellow,
+                    fontSize: 25.sp,
+                    fontWeight: FontWeight.bold,
+                  ),
+                  textAlign: TextAlign.center,
+                ),
+                SizedBox(height: 20.h),
+                Text(
+                  "An error occured while creating your profile. Please try again later.",
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 18.sp,
+                  ),
+                  textAlign: TextAlign.center,
+                ),
+                SizedBox(height: 30.h),
+                ZoomTapAnimation(
+                  onTap: () {
+                    playTap(context);
+                    Navigator.pop(context);
+                  },
+                  child: Container(
+                    padding: EdgeInsets.symmetric(
+                      vertical: 10.sp,
+                      horizontal: 20.sp,
+                    ),
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(20.r),
+                    ),
+                    child: Text(
+                      "Okay",
+                      style: TextStyle(
+                        fontSize: 20.sp,
+                      ),
+                    ),
+                  ),
+                )
+              ],
+            ),
+          );
         });
       }
     }).catchError((error) {
       print("Error checking player existence: $error");
+
+      showGameDialog(
+        context,
+        isExitable: true,
+        padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 40),
+        margin: EdgeInsets.symmetric(horizontal: 60.w, vertical: 24.h),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Text(
+              "",
+              style: TextStyle(
+                color: AppColor.yellow,
+                fontSize: 25.sp,
+                fontWeight: FontWeight.bold,
+              ),
+              textAlign: TextAlign.center,
+            ),
+            SizedBox(height: 20.h),
+            Text(
+              "Your profile has been created successfully. Your can now view your progress on the leaderboard.",
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 18.sp,
+              ),
+              textAlign: TextAlign.center,
+            ),
+            SizedBox(height: 30.h),
+            ZoomTapAnimation(
+              onTap: () {
+                playTap(context);
+                Navigator.pop(context);
+              },
+              child: Container(
+                padding: EdgeInsets.symmetric(
+                  vertical: 10.sp,
+                  horizontal: 20.sp,
+                ),
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(20.r),
+                ),
+                child: Text(
+                  "Okay",
+                  style: TextStyle(
+                    fontSize: 20.sp,
+                  ),
+                ),
+              ),
+            )
+          ],
+        ),
+      );
     });
 
     notifyListeners();
