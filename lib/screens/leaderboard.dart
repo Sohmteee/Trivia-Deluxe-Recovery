6b59d652b6d5c327a8c5ee799940da43061ff2be
Ukaegbe Somtochukwu <sohmteecodes@gmail.com>
2023-10-29 08:15:10 +0100

    commit

--- lib/screens/leaderboard.dart
+++ lib/screens/leaderboard.dart
@@ -270,7 +270,7 @@ class _LeaderBoardScreenState extends State<LeaderBoardScreen>
                                   3,
                                   (index) {
                                     return FutureBuilder(
-                                      future: getLeaderBoardData(index),
+                                      future: getLeaderBoardData(0),
                                       initialData: null,
                                       builder: (BuildContext context,
                                           AsyncSnapshot snapshot) {
