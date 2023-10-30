9d07c32cec41e5b7edfc1ff0d2c00f6d15159a3d
Ukaegbe Somtochukwu <sohmteecodes@gmail.com>
2023-10-30 13:45:34 +0100

    commit

--- lib/screens/streaks.dart
+++ lib/screens/streaks.dart
@@ -380,7 +380,7 @@ class _StreaksScreeenState extends State<StreaksScreeen> {
                               return Column(
                                 children: [
                                   ListTile(
-                                    minVerticalPadding: 20,
+                                    minVerticalPadding: 2.h,
                                     minLeadingWidth: 20.w,
                                     leading: item["status"]
                                         ? Image.asset(
