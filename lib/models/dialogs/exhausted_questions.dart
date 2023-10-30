ccce896f731f51210ed063aa9c7dec1c4aa4d67c
Ukaegbe Somtochukwu <sohmteecodes@gmail.com>
2023-10-30 19:03:18 +0100

    commit

--- lib/models/dialogs/exhausted_questions.dart
+++ lib/models/dialogs/exhausted_questions.dart
@@ -1,9 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_animate/flutter_animate.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:trivia/colors/app_color.dart';
-import 'package:trivia/main.dart';
-import 'package:zoom_tap_animation/zoom_tap_animation.dart';
 
 import 'game.dart';
 
@@ -34,32 +31,6 @@ showExhaustedQuestionsDialog(BuildContext context) {
           ),
           textAlign: TextAlign.center,
         ),
-        SizedBox(height: 30.h),
-        ZoomTapAnimation(
-          onTap: () {
-            playTap(context);
-            Future.delayed(
-              3.microseconds,
-              () => Navigator.pushNamed(context, "/stage"),
-            );
-          },
-          child: Container(
-            padding: EdgeInsets.symmetric(
-              vertical: 10.sp,
-              horizontal: 20.sp,
-            ),
-            decoration: BoxDecoration(
-              color: Colors.white,
-              borderRadius: BorderRadius.circular(20.r),
-            ),
-            child: Text(
-              "Okay",
-              style: TextStyle(
-                fontSize: 20.sp,
-              ),
-            ),
-          ),
-        )
       ],
     ),
   );
