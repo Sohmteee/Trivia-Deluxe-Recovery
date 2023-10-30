5d4e494c9258424d58ac6819f0dfbf917d52bea4
Ukaegbe Somtochukwu <sohmteecodes@gmail.com>
2023-10-31 00:03:52 +0100

    commit

--- lib/screens/select.dart
+++ lib/screens/select.dart
@@ -1,10 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_animate/flutter_animate.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:google_mobile_ads/google_mobile_ads.dart';
 import 'package:provider/provider.dart';
-import 'package:toast/toast.dart';
-import 'package:trivia/ad_helper.dart';
 import 'package:trivia/colors/app_color.dart';
 import 'package:trivia/data/box.dart';
 import 'package:trivia/data/questions/anime.dart';
@@ -130,9 +127,6 @@ class _SelectScreenState extends State<SelectScreen> {
       }
     }
 
-    /* _initGoogleMobileAds();
-    _loadBannerAd(); */
-
     final selectProvider = Provider.of<SelectProvider>(context, listen: false);
     pageController = PageController(initialPage: selectProvider.pageIndex);
     super.initState();
@@ -153,7 +147,6 @@ class _SelectScreenState extends State<SelectScreen> {
         return true;
       },
       child: GameBackground(
-        
         body: Padding(
           padding: EdgeInsets.symmetric(vertical: 40.h),
           child: Column(
@@ -174,38 +167,7 @@ class _SelectScreenState extends State<SelectScreen> {
               const Spacer(flex: 2),
               buildCategories(),
               const Spacer(flex: 2),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  for (int i = 0; i < selectItems.length; i++)
-                    ZoomTapAnimation(
-                      onTap: () {
-                        final selectProvider =
-                            Provider.of<SelectProvider>(context, listen: false);
-                        pageController.animateToPage(
-                          i,
-                          duration: .3.seconds,
-                          curve: Curves.easeOut,
-                        );
-                        selectProvider.pageIndex = i;
-                      },
-                      child: AnimatedContainer(
-                        duration: .3.seconds,
-                        margin: EdgeInsets.symmetric(horizontal: 5.sp),
-                        width: 15.sp,
-                        height: 15.sp,
-                        decoration: BoxDecoration(
-                          color:
-                              (Provider.of<SelectProvider>(context).pageIndex ==
-                                      i)
-                                  ? AppColor.yellow
-                                  : AppColor.lightRed.withOpacity(.5),
-                          borderRadius: BorderRadius.circular(50.r),
-                        ),
-                      ),
-                    ),
-                ],
-              ),
+              categoriesIndicators(context),
               const Spacer(),
             ],
           ),
@@ -214,184 +176,176 @@ class _SelectScreenState extends State<SelectScreen> {
     );
   }
 
+  Row categoriesIndicators(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        for (int i = 0; i < selectItems.length; i++)
+          ZoomTapAnimation(
+            onTap: () {
+              final selectProvider =
+                  Provider.of<SelectProvider>(context, listen: false);
+              pageController.animateToPage(
+                i,
+                duration: .3.seconds,
+                curve: Curves.easeOut,
+              );
+              selectProvider.pageIndex = i;
+            },
+            child: AnimatedContainer(
+              duration: .3.seconds,
+              margin: EdgeInsets.symmetric(horizontal: 5.sp),
+              width: 15.sp,
+              height: 15.sp,
+              decoration: BoxDecoration(
+                color: (Provider.of<SelectProvider>(context).pageIndex == i)
+                    ? AppColor.yellow
+                    : AppColor.lightRed.withOpacity(.5),
+                borderRadius: BorderRadius.circular(50.r),
+              ),
+            ),
+          ),
+      ],
+    );
+  }
+
   SizedBox buildCategories() {
     return SizedBox(
-              height: 400.h,
-              child: Consumer<SelectProvider>(
-                  builder: (context, selectProvider, _) {
-                return PageView.builder(
-                    itemCount: selectItems.length,
-                    controller: pageController,
-                    physics: const BouncingScrollPhysics(),
-                    scrollDirection: Axis.horizontal,
-                    onPageChanged: (value) {
-                      selectProvider.pageIndex = value;
-                    },
-                    itemBuilder: (context, pageIndex) {
-                      final selectItem = selectItems[pageIndex];
+      height: 400.h,
+      child: Consumer<SelectProvider>(builder: (context, selectProvider, _) {
+        return PageView.builder(
+            itemCount: selectItems.length,
+            controller: pageController,
+            physics: const BouncingScrollPhysics(),
+            scrollDirection: Axis.horizontal,
+            onPageChanged: (value) {
+              selectProvider.pageIndex = value;
+            },
+            itemBuilder: (context, pageIndex) {
+              final selectItem = selectItems[pageIndex];
 
-                      return ListView.builder(
-                        itemCount: selectItems.length,
-                        padding: EdgeInsets.only(
-                            top: 20.h, left: 10.sp, right: 10.sp),
-                        physics: const NeverScrollableScrollPhysics(),
-                        itemBuilder: (BuildContext context, int listIndex) {
-                          final item = selectItem[listIndex];
+              return ListView.builder(
+                itemCount: selectItems.length,
+                padding: EdgeInsets.only(top: 20.h, left: 10.sp, right: 10.sp),
+                physics: const NeverScrollableScrollPhysics(),
+                itemBuilder: (BuildContext context, int listIndex) {
+                  final item = selectItem[listIndex];
 
-                          return Consumer<QuestionProvider>(
-                              builder: (_, questionProvider, child) {
-                            return Padding(
-                              padding: EdgeInsets.symmetric(horizontal: 20.w),
-                              child: ZoomTapAnimation(
-                                onTap: () {
-                                  var stageProvider =
-                                      Provider.of<StageProvider>(context,
-                                          listen: false);
-                                  stageProvider.resetCompletedStage();
-                                  questionProvider.title =
-                                      item["data"]["title"];
+                  return Consumer<QuestionProvider>(
+                      builder: (_, questionProvider, child) {
+                    return Padding(
+                      padding: EdgeInsets.symmetric(horizontal: 20.w),
+                      child: ZoomTapAnimation(
+                        onTap: () {
+                          var stageProvider = Provider.of<StageProvider>(
+                              context,
+                              listen: false);
+                          stageProvider.resetCompletedStage();
+                          questionProvider.title = item["data"]["title"];
 
-                                  if (box.get(item["data"]["title"]) ==
-                                      null) {
-                                    box.put(
-                                        item["data"]["title"], item["data"]);
-                                  }
+                          if (box.get(item["data"]["title"]) == null) {
+                            box.put(item["data"]["title"], item["data"]);
+                          }
 
-                                  questionProvider.data =
-                                      box.get(item["data"]["title"]);
+                          questionProvider.data =
+                              box.get(item["data"]["title"]);
 
-                                  questionProvider.currentLevel = box.get(
-                                      item["data"]["title"])["currentLevel"];
+                          questionProvider.currentLevel =
+                              box.get(item["data"]["title"])["currentLevel"];
 
-                                  if (questionProvider.currentLevel == 30) {
-                                    showCompletedCategoryDialog(context);
-                                  } else {
-                                    Future.delayed(
-                                      3.microseconds,
-                                      () => Navigator.pushNamed(
-                                          context, "/stage"),
-                                    );
-                                  }
-                                },
-                                child: Container(
-                                  padding: EdgeInsets.all(10.sp),
-                                  margin: EdgeInsets.only(bottom: 20.sp),
-                                  decoration: BoxDecoration(
-                                    color: AppColor.lightRed,
-                                    border: Border.all(
-                                      width: 2.sp,
-                                      color: AppColor.lightRed,
-                                    ),
-                                    borderRadius: BorderRadius.circular(20.r),
-                                  ),
-                                  child: ListTile(
-                                    leading: Image.asset(
-                                      item["image"],
-                                      width: item["data"]["title"] ==
-                                              "Proverbs, Idioms, and Riddles"
-                                          ? 35.w
-                                          : 40.w,
-                                      height: item["data"]["title"] ==
-                                              "Proverbs, Idioms, and Riddles"
-                                          ? 35.h
-                                          : 40.h,
-                                    ),
-                                    title: Text(
-                                      item["data"]["title"],
-                                      style: TextStyle(
-                                        color: AppColor.white,
-                                        fontSize: 16.sp,
-                                      ),
-                                      textAlign: TextAlign.center,
-                                    ),
-                                    trailing: Column(
-                                      mainAxisAlignment:
-                                          MainAxisAlignment.center,
-                                      children: [
-                                        Text(
-                                          "Level ${box.get(item["data"]["title"])?["currentLevel"]} / 30",
-                                          style: TextStyle(
-                                            fontSize: 16.sp,
-                                            color: AppColor.yellow,
-                                          ),
-                                        ),
-                                        SizedBox(height: 5.h),
-                                        Stack(
-                                          alignment: Alignment.centerLeft,
-                                          children: [
-                                            Container(
-                                              height: 6.h,
-                                              width: 50.toDouble(),
-                                              decoration: BoxDecoration(
-                                                color: AppColor.white,
-                                                borderRadius:
-                                                    BorderRadius.circular(
-                                                        50.r),
-                                              ),
-                                            ),
-                                            Container(
-                                              height: 6.h,
-                                              width: (box.get(item["data"]
-                                                                  ["title"])?[
-                                                              "currentLevel"] ==
-                                                          0
-                                                      ? 0
-                                                      : box.get(item["data"]
-                                                                  ["title"])?[
-                                                              "currentLevel"] +
-                                                          3) /
-                                                  33 *
-                                                  50.toDouble(),
-                                              decoration: BoxDecoration(
-                                                color: AppColor.yellow,
-                                                borderRadius:
-                                                    BorderRadius.circular(
-                                                        50.r),
-                                              ),
-                                            ),
-                                          ],
-                                        ),
-                                      ],
-                                    ),
+                          if (questionProvider.currentLevel == 30) {
+                            showCompletedCategoryDialog(context);
+                          } else {
+                            Future.delayed(
+                              3.microseconds,
+                              () => Navigator.pushNamed(context, "/stage"),
+                            );
+                          }
+                        },
+                        child: Container(
+                          padding: EdgeInsets.all(10.sp),
+                          margin: EdgeInsets.only(bottom: 20.sp),
+                          decoration: BoxDecoration(
+                            color: AppColor.lightRed,
+                            border: Border.all(
+                              width: 2.sp,
+                              color: AppColor.lightRed,
+                            ),
+                            borderRadius: BorderRadius.circular(20.r),
+                          ),
+                          child: ListTile(
+                            leading: Image.asset(
+                              item["image"],
+                              width: item["data"]["title"] ==
+                                      "Proverbs, Idioms, and Riddles"
+                                  ? 35.w
+                                  : 40.w,
+                              height: item["data"]["title"] ==
+                                      "Proverbs, Idioms, and Riddles"
+                                  ? 35.h
+                                  : 40.h,
+                            ),
+                            title: Text(
+                              item["data"]["title"],
+                              style: TextStyle(
+                                color: AppColor.white,
+                                fontSize: 16.sp,
+                              ),
+                              textAlign: TextAlign.center,
+                            ),
+                            trailing: Column(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                Text(
+                                  "Level ${box.get(item["data"]["title"])?["currentLevel"]} / 30",
+                                  style: TextStyle(
+                                    fontSize: 16.sp,
+                                    color: AppColor.yellow,
                                   ),
                                 ),
-                              ),
-                            );
-                          });
-                        },
-                      );
-                    });
-              }),
-            );
-  }
-
-  Future<InitializationStatus> _initGoogleMobileAds() {
-    return MobileAds.instance.initialize();
-  }
-
-  _loadBannerAd() {
-    BannerAd(
-      adUnitId: AdHelper.bannerAdUnitId,
-      request: const AdRequest(),
-      size: AdSize.banner,
-      listener: BannerAdListener(
-        onAdLoaded: (ad) {
-          setState(() {
-            // _bannerAd = ad as BannerAd;
-          });
-        },
-        onAdFailedToLoad: (ad, err) {
-          print('Failed to load a banner ad: ${err.message}');
-          Toast.show("Failed to load ad",
-              textStyle: TextStyle(
-                fontSize: 20.sp,
-                color: Colors.white,
-              ),
-              duration: 3,
-              gravity: Toast.bottom);
-          ad.dispose();
-        },
-      ),
-    ).load();
+                                SizedBox(height: 5.h),
+                                Stack(
+                                  alignment: Alignment.centerLeft,
+                                  children: [
+                                    Container(
+                                      height: 6.h,
+                                      width: 50.toDouble(),
+                                      decoration: BoxDecoration(
+                                        color: AppColor.white,
+                                        borderRadius:
+                                            BorderRadius.circular(50.r),
+                                      ),
+                                    ),
+                                    Container(
+                                      height: 6.h,
+                                      width: (box.get(item["data"]["title"])?[
+                                                      "currentLevel"] ==
+                                                  0
+                                              ? 0
+                                              : box.get(item["data"]["title"])?[
+                                                      "currentLevel"] +
+                                                  3) /
+                                          33 *
+                                          50.toDouble(),
+                                      decoration: BoxDecoration(
+                                        color: AppColor.yellow,
+                                        borderRadius:
+                                            BorderRadius.circular(50.r),
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ],
+                            ),
+                          ),
+                        ),
+                      ),
+                    );
+                  });
+                },
+              );
+            });
+      }),
+    );
   }
 }
