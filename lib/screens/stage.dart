0367653def741fec7405ab0206c0ee97ad3e5a6b
Ukaegbe Somtochukwu <sohmteecodes@gmail.com>
2023-10-31 15:32:29 +0100

    commit

--- lib/screens/stage.dart
+++ lib/screens/stage.dart
@@ -54,33 +54,31 @@ class _StageScreenState extends State<StageScreen> {
         return true;
       },
       child: GameBackground(
-        body: Column(
-          children: [
-            Column(
-              children: [
-                const GameStats(),
-                const Spacer(),
-                levelTitle(),
-                const Spacer(),
-                buildLevel(),
-                const Spacer(flex: 3),
-                playButton(),
-              ],
-            ),
-            SizedBox(height: 20.h),
-            SizedBox(
-              width: _bannerAd!.size.width.toDouble(),
-              height: _bannerAd!.size.height.toDouble(),
-              child: AdWidget(ad: _bannerAd!),
-            ),
-          ],
+        body: Padding(
+          padding: EdgeInsets.fromLTRB(40.w, 40.h, 40.w, 0),
+          child: Column(
+            children: [
+              const GameStats(),
+              const Spacer(),
+              levelTitle(),
+              const Spacer(),
+              buildLevel(),
+              const Spacer(flex: 3),
+              playButton(),
+              SizedBox(height: 20.h),
+              SizedBox(
+                width: _bannerAd!.size.width.toDouble(),
+                height: _bannerAd!.size.height.toDouble(),
+                child: AdWidget(ad: _bannerAd!),
+              ),
+            ],
+          ),
         ),
       ),
     );
   }
 
   _loadBannerAd() {
-    
     _bannerAd = BannerAd(
       adUnitId: AdHelper.bannerAdUnitId,
       request: const AdRequest(),
