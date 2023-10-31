588367ad24f6e042a8a21776b52caf0010a6c0bd
Ukaegbe Somtochukwu <sohmteecodes@gmail.com>
2023-10-31 23:15:08 +0100

    commit

--- lib/models/dialogs/low_cash.dart
+++ lib/models/dialogs/low_cash.dart
@@ -33,7 +33,6 @@ loadRewardedAd(BuildContext context) {
       onAdFailedToLoad: (err) {
         print('Failed to load a rewarded ad: ${err.message}');
         Navigator.pushReplacementNamed(context, "/stage");
-        playBGAudio(context);
       },
     ),
   );
