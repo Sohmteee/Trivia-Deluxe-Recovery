c636659dd73f4c8d41a37bcafc819e86af94898a
Ukaegbe Somtochukwu <sohmteecodes@gmail.com>
2023-11-01 10:44:21 +0100

    commit

--- lib/screens/reward.dart
+++ lib/screens/reward.dart
@@ -573,8 +573,7 @@ class _RewardScreenState extends State<RewardScreen> {
         // Called when an ad request failed.
         onAdFailedToLoad: (ad, err) {
           debugPrint('BannerAd failed to load: $err');
-          // Dispose the ad here to free resources.
-          ad.dispose();
+          _loadBannerAd();
         },
       ),
     )..load();
