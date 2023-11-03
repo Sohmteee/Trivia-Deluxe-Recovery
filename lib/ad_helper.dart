43758213c198d887f0a980dc21bbfbdd4ccf28d5
Ukaegbe Somtochukwu <sohmteecodes@gmail.com>
2023-11-03 12:49:16 +0100

    commit

--- lib/ad_helper.dart
+++ lib/ad_helper.dart
@@ -3,20 +3,6 @@ import 'dart:io';
 import 'package:flutter/foundation.dart';
 
 class AdHelper {
-  static String get bannerAdUnitId {
-    if (Platform.isAndroid) {
-      if (kDebugMode) {
-        return 'ca-app-pub-3940256099942544/6300978111';
-      } else {
-        return 'ca-app-pub-4696224049420135/8371135025';
-      }
-    } else if (Platform.isIOS) {
-      return 'ca-app-pub-3940256099942544/2934735716';
-    } else {
-      throw UnsupportedError('Unsupported platform');
-    }
-  }
-
   static String get interstitialAdUnitId {
     if (Platform.isAndroid) {
       if (kDebugMode) {
