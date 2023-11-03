6ddd352a49913043c0e21ce646b3710be22455c7
Ukaegbe Somtochukwu <sohmteecodes@gmail.com>
2023-11-03 12:54:43 +0100

    commit

--- lib/screens/menu.dart
+++ lib/screens/menu.dart
@@ -132,7 +132,7 @@ class _MenuScreenState extends State<MenuScreen>
                         ),
                     const Spacer(flex: 3),
                     bottomButtons(context),
-                    const Spacer(flex: 2),
+                    const Spacer(flex: 1),
                   ],
                 ),
               ),
