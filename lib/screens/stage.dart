3a33ea6abce5bf4cb9a15b4e930d987cfab909ca
Ukaegbe Somtochukwu <sohmteecodes@gmail.com>
2023-11-03 16:42:39 +0100

    commit

--- lib/screens/stage.dart
+++ lib/screens/stage.dart
@@ -159,7 +159,7 @@ class _StageScreenState extends State<StageScreen> {
         ),
         Positioned(
           bottom: -40.h,
-          right: 75.w,
+          right: 80.w,
           child: SizedBox(
             height: 150.h,
             child: Lottie.asset(
